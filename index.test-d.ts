1165c8e1476f91c927b318dd8d6c657bb2707011
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-13 14:46:55 -0700

    fix: avoid `base` to be set to an intersection with `void` if a plugin returns `void`

--- index.test-d.ts
+++ index.test-d.ts
@@ -3,20 +3,56 @@ import { Base } from "./index.js";
 
 const fooPlugin = () => {
   return {
-    foo: () => "foo",
+    foo: "foo",
+  };
+};
+const barPlugin = () => {
+  return {
+    bar: "bar",
   };
 };
 
-const MyBase = Base.plugin(fooPlugin).defaults({
-  default: "value",
-});
-const base = new MyBase({
-  option: "value",
-});
+const voidPlugin = () => {
+  // returns void
+};
 
-expectType<string>(base.options.default);
-expectType<string>(base.options.option);
-expectType<string>(base.foo());
+const base = new Base();
 
 // @ts-expect-error unknown properties cannot be used, see #31
 base.unknown;
+
+const FooBase = Base.plugin(fooPlugin).defaults({
+  default: "value",
+});
+const fooBase = new FooBase({
+  option: "value",
+});
+
+expectType<string>(fooBase.options.default);
+expectType<string>(fooBase.options.option);
+expectType<string>(fooBase.foo);
+
+const BaseWithVoidPlugin = Base.plugin(voidPlugin);
+const baseWithVoidPlugin = new BaseWithVoidPlugin();
+
+// @ts-expect-error unknown properties cannot be used, see #31
+baseWithVoidPlugin.unknown;
+
+const BaseWithFooAndBarPlugins = Base.plugin(barPlugin, fooPlugin);
+const baseWithFooAndBarPlugins = new BaseWithFooAndBarPlugins();
+
+expectType<string>(baseWithFooAndBarPlugins.foo);
+expectType<string>(baseWithFooAndBarPlugins.bar);
+
+// @ts-expect-error unknown properties cannot be used, see #31
+baseWithFooAndBarPlugins.unknown;
+
+const BaseWithVoidAndNonVoidPlugins = Base.plugin(
+  barPlugin,
+  voidPlugin,
+  fooPlugin
+);
+const baseWithVoidAndNonVoidPlugins = new BaseWithVoidAndNonVoidPlugins();
+
+expectType<string>(baseWithVoidAndNonVoidPlugins.foo);
+expectType<string>(baseWithVoidAndNonVoidPlugins.bar);
